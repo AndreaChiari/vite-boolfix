d8948fbb482d17c197583e3814ca1b7c85d1e730
AndreaChiari <chiariandrea94@gmail.com>
2023-01-26 19:33:25 +0100

    update-title-norepetition

--- src/components/main.vue
+++ src/components/main.vue
@@ -10,22 +10,22 @@ export default {
     },
     data() {
         return {
-
             en: 'en.png',
             it: 'it.png'
-
         }
+
     }
+
 }
 </script>
 <template>
     <h1>{{ title }}</h1>
-    <h2>{{ originalTitle }}</h2>
+    <h2 v-show="originalTitle !== title">{{ originalTitle }}</h2>
     <div>
-        <img v-if="language === 'it'" src="../img/it.png" alt="">
-        <img v-if="language === 'en'" src="../img/en.png" alt="">
+        <img v-if="language === 'it'" src="../img/it.png" alt="it">
+        <img v-if="language === 'en'" src="../img/en.png" alt="en">
     </div>
-    <p>{{ vote }} </p>
+    <p>{{ vote }}</p>
 </template>
 <style>
 
